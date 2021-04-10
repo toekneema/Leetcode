ec37efd8bbfbc5259841bb7acd65dc2d63c30aa9
Tony Ma <42778379+toekneema@users.noreply.github.com>
2021-04-10 16:31:04 -0400

    Update BellmanFord.py

--- BellmanFord.py
+++ BellmanFord.py
@@ -2,21 +2,21 @@ edges = []
 dic = defaultdict(int)
 allCurrs = set()
 for s,t,r in zip(sources,targets,rates):
-	dic[s,t] = r
-	edges.append([s,t,r])
-	allCurrs.add(s)
-        allCurrs.add(t)
+    dic[s,t] = r
+    edges.append([s,t,r])
+    allCurrs.add(s)
+    allCurrs.add(t)
 
 bestAmts = {curr : float('-inf') for curr in allCurrs}
 bestAmts[source] = 1
         
 for i in range(len(allCurrs)-1):
-	for s,t,r in edges: # source/target
-		newAmt = bestAmts[s] * r
-                if newAmt > bestAmts[t]:
-                    bestAmts[t] = newAmt
-
-for s,t,r in edges:
+    for s,t,r in edges: # source/target
 	newAmt = bestAmts[s] * r
         if newAmt > bestAmts[t]:
-		return -1
+            bestAmts[t] = newAmt
+
+for s,t,r in edges:
+    newAmt = bestAmts[s] * r
+    if newAmt > bestAmts[t]:
+	return -1
